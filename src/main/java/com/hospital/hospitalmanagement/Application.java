a115dabbd16536ecc3287f146645f3e463d2ea8d
Chandrika-Karri <chandrika.kovvuri@gmail.com>
2025-04-08 22:42:05 +0200

    initial commit

--- src/main/java/com/hospital/hospitalmanagement/Application.java
+++ src/main/java/com/hospital/hospitalmanagement/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+
+     //   System.out.println("Database URL: " + System.getenv("RDS_HOSTNAME"));
+
         SpringApplication.run(Application.class, args);
     }
 
